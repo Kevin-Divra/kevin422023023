2814b2e571d47cd6ae93526dcdf9a657d3da3eb9
Kevin-Divra <151398701+Kevin-Divra@users.noreply.github.com>
2024-06-07 02:09:51 +0700

    semoga diberi kekuatan

---
+++ resources/views/pages/home.blade.php
@@ -0,0 +1,127 @@
+@extends('layouts.app-public')
+@section('title', 'Home')
+@section('content')
+    <div class="site-wrapper-reveal">
+        <div class="hero-box-area">
+            <div class="countainer">
+                <div class="row">
+                    <div class="col-lg-12">
+                        <!-- Hero Slider Area Start -->
+                        <div class="hero-area" id="product-preview">
+                        </div>
+                        <!-- Hero Slider Area End -->
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="about-us-area section-space--ptb_120">
+            <div class="countainer">
+                <div class="row">
+                    <div class="col-lg-12">
+                        <div class="about-us-content_6 text-center">
+                            <h2>VINN&nbsp;&nbsp;Sport</h2>
+                            <p>
+                                <small>
+                                Welcome to the best sports store! We offer a wide range of products from top brands designed to support every step of your journey to success. Find the perfect gear for every sport.Stay at the top of your game with the latest and most innovative products from our store. We pride ourselves on offering the newest technology and best designs to ensure you get the most out of every workout and competition.
+                                </small>
+                            </p>
+                            <P class="mt-5">Unleash Your Potential with
+                                <span class="text-color-primary"> Premium Sports Equipment!</span>
+                            </P>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div> 
+
+        <!-- Banner Video Area Start -->
+        <div class="banner-video-area overflow-hidden">
+            <div class="container">
+                <div class="row">
+                    <div class="col-lg-12">
+                        <div class="banner-video-box">
+                            <img src="https://www.magd.ox.ac.uk/wp-content/uploads/2022/07/3027-052-2000x1000.jpg" alt="">
+                            <div class="video-icon">
+                                <a href="https://youtu.be/Na5KPnx0uS8?si=PPoJfum8xG-Jt8h9" class="popup-youtube"><i class="linear-ic-play"></i></a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- Banner Video Area End -->
+
+        <!-- Our Brand Area Start -->
+        <div class="our-brand-area section-space--pb_90">
+            <div class="container">
+                <div class="brand-slider-active">
+                    @php
+                        $partner_count = 8;
+                    @endphp
+                    @for($i=1;$i<=$partner_count;$i++)
+                        <div class="col-lg-12">
+                            <div class="single-brand-item">
+                                <a href="#"><img src="assets/images/brand/partner{{ $i }}.jpg" class="img-fluid" alt="Partner Images"></a>
+                            </div>
+                        </div>
+                    @endfor
+                </div>
+            </div>
+        </div>
+        <!-- Our Brand Area End -->
+
+        <!-- Our Member Area Start -->
+        <div class="our-member-area section-space--pb_120">
+
+            <div class="container">
+                <div class="row">
+                    <div class="col-lg-12">
+                        <div class="member--box">
+                            <div class="row align-items-center">
+                                <div class="col-lg-5 col-md-4">
+                                    <div class="section-title small-mb__40 tablet-mb__40">
+                                        <h4 class="section-title">Join the community!</h4>
+                                        <p>Become one of the member and get discount 50% off</p>
+                                    </div>
+                                </div>
+                                <div class="col-lg-7 col-md-8">
+                                    <div class="member-wrap">
+                                        <form action="#" class="member--two">
+                                            <input class="input-box" type="text" placeholder="Your email address">
+                                            <button class="submit-btn"><i class="icon-arrow-right"></i></button>
+                                        </form>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- Our Member Area End -->
+          
+    </div>
+@endsection
+@section('addition_css')
+@endsection
+@section('addition_script')
+    <script src="{{asset('pages/js/home.js')}}"></script>
+@endsection
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
